64762774cbc8eba6a82f502b05df0e5e4182cb6c
Wagner da Silva Aquino Junior <wagner.aquino@tecnospeed.com.br>
2023-04-06 20:23:18 -0300

    inserindo laço de repetição - for/while

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
-// Pom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// laço de repetiação for
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`
+
+    //utilizando função anônima
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+}
+
+/* laço de repetição while
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    contador++;
+}
+*/ 
